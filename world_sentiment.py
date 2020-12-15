1870a0e7f3775bb3e151b14911dec3218dc70749
arijit1201 <36472612+arijit1201@users.noreply.github.com>
2020-12-16 05:02:14 +0530

    added python code

---
+++ world_sentiment.py
@@ -0,0 +1,20 @@
+import tweepy
+import textblob as tb
+import preprocessor
+import statistics
+from typing import List
+
+print("It is running\n")
+
+def get_tweets():
+    return
+
+def clean_tweets():
+    return
+
+def get_sentiment():
+    return
+
+def generate_avg_sentiment_score():
+    return
+
